1c5a0e81a6b42a46a5d46e112a5df8143d541a0a
Fernmu <35671996+Fernmu@users.noreply.github.com>
2018-02-25 18:48:48 -0600

    Update hanoiMIO.asm

--- hanoiMIO.asm
+++ hanoiMIO.asm
@@ -1,6 +1,9 @@
 #autores: Fernanda Muñoz y luis Fernando 
 #20/02/2018 - 25/02/2018
 
+     						#NO FUNCIONA AUN 
+  
+  
 .text
  
  	addi $s0, $0, 8 	 	#numero de discos
@@ -35,7 +38,7 @@
  	
  ELSE:
  
- 
+
 	lw $t0, ($s1)			# carga en un temporal el valor de la torre de inicio
  	addi $sp, $sp, -20 		#se le quita espacio al stack para poder trabajar ahí
 	sw $ra, 20($sp) 		#guardamos 1 byte de espacio para ra en el stack
@@ -43,7 +46,7 @@
 	sw $s1, 8($sp) 			#guardamos 1 byte de espacio para la torre origen en el stack
 	sw $s2, 4($sp) 			#guardamos 1 byte de espacio para la torre auxiliar en el stack
 	sw $s3, 0($sp) 			#guardamos 1 byte de espacio para la torre destino en el stack
-	addi $s0, $s0, -1 		#le restamos 1 disco a la pila de s0
+	addi $s0, $s0, -1 		#le restamos 1 disco a la pila de n
 
  	
  	
@@ -51,35 +54,36 @@
 	add $s2, $s3, $0		#intercambio valores de torre auxiliar con torre destino
 	add $s3, $t1, $0
 	
-	jal HANOI 			#llamamos a HANOI/ recursividad 
+	jal HANOI 			#jump and link a HANOI/ recursividad 
 
 	add $t1, $s2, $0 		#repito proceso anterior
 	add $s2, $s3, $0		
 	add $s3, $t1, $0
 	
-	lw $t2, -4($s1) 		
-	sw $0, -4($s1) 			
-	sw $t2, 0($s3) 			
-	addi $s3, $s3, 4 		
-	addi $s1, $s1, -4
+	lw $t2, -4($s1) 		#le carga al temporal 2 el disco de arriba de la torre auziliar 
+	sw $0, -4($s1) 			#borra el disco de arriba que cargamos en el temporal
+	sw $t2, 0($s3) 			#guardamos el disco que estaba en el temporal en la torre destino
+	addi $s3, $s3, 4 		#se cambia el apuntador del disco (se mueve un byte)
+	addi $s1, $s1, -4		#se borra un disco de la torre origen.
 	
-	add $t1, $s2, $0
-	add $s2, $s1, $0
-	add $s1, $t1, $0
+	add $t1, $s2, $0		#guardo s2 en un temporal 
+	add $s2, $s1, $0		#guardo en s2 lo que hay en s1
+	add $s1, $t1, $0		#guardo en s1 el temporal donde guarde s2 (switcheo)
 
 	jal HANOI
 	
-	add $t1, $s2, $0
-	add $s2, $s1, $0
-	add $s1, $t1, $0
+					#recupero entorno 
+	add $t1, $s2, $0		#guardo s2 en el temporal 
+	add $s2, $s1, $0		#guardo en s2 lo que esta en s1
+	add $s1, $t1, $0		#guardo en s1 lo que esta en el temporal
 	
-	lw $ra, 4($sp) 		
-	lw $s0, 0($sp) 		
-	addi $sp, $sp, 8 	
+	lw $ra, 20($sp) 		#carga lo que esta en el stack a ra (en la dirección con 4 bytes)
+	lw $s0, 16($sp) 		#carga lo que esta en el stack al registo s0  (en la direccion con 0)
+	lw $s1, 12($sp) 		#carga lo que esta en el stack al registro s1 (en la direccion con 0)
+	lw $s2, 8($sp) 			#carga lo que esta en el stack al registro s2 (en la direccion con 0)
+	lw $s3, 0($sp) 			#carga lo que esta en el stack al registro s3    (en la direccion con 0)
+	addi $sp, $sp, 20		#libera la memoria del stack que utilizamos  	
 
 	jr $ra
- 	
- 	
-
  
  EXIT: 
